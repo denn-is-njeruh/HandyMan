cf3220415d502d3b9231acf00200ab72f4f8bc38
denn-is-njeruh <gitonjeruh@gmail.com>
2021-10-01 10:20:58 +0300

    add custom user admin and register the User and custom user admin to enable admin to use the forms by subclassing UserAdmin

--- handyworks/admin.py
+++ handyworks/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .forms import NewUserCreationForm,ExistingUserChangeForm
+from .models import User
 
 # Register your models here.
+class NewUserAdmin(UserAdmin):
+	add_form = NewUserCreationForm
+	form = ExistingUserChangeForm
+	model = User
+	list_display = ('email','is_staff','is_active',)
+	list_filter = ('email', 'is_staff','is_active',)
+	fieldsets = (
+		(None, {
+			'classes': ('wide',),
+			'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active')}
+		),
+	)
+	search_fields = ('email',)
+	ordering = ('email',)
+
+
+admin.site.register(User, NewUserAdmin)
