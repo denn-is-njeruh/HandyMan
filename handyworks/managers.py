ae1b0ea235eb9fa05feaba4421f7a57c43d38a56
denn-is-njeruh <gitonjeruh@gmail.com>
2021-10-01 08:24:09 +0300

    add create super user method to the custom User model manager

--- handyworks/managers.py
+++ handyworks/managers.py
@@ -3,19 +3,32 @@ from django.utils.translation import ugettext_lazy as _
 
 #Your class manager here
 class UserManager(BaseUserManager):
-  """
-    Custom user model manager where email is the unique identifiers
-    for authentication instead of usernames.
-  """
-  def create_user(self,email, password, **extra_fields):
-    """
-      Create and save a User with the given email and password.
-    """
-    if not email:
-      raise ValueError('User must set an email to proceed')
-    #extra_fields.setdefault('is_superuser',False)
-    email = self.normalize_email(email)
-    user = self.model(email=email,**extra_fields)
-    user.set_password(password)
-    user.save(using=self._db)
-    return user
+	"""
+		Custom user model manager where email is the unique identifiers
+		for authentication instead of usernames.
+	"""
+	def create_user(self,email, password, **extra_fields):
+		"""
+		Create and save a User with the given email and password.
+		"""
+		if not email:
+			raise ValueError('User must set an email to proceed')
+		email = self.normalize_email(email)
+		user = self.model(email=email,**extra_fields)
+		user.set_password(password)
+		user.save(using=self._db)
+		return user
+
+	def create_superuser(self,email,password=None,**extra_fields):
+		"""
+		Create and save a SuperUser with the given email and password.
+		"""
+		extra_fields.setdefault('is_staff', True)
+		extra_fields.setdefault('is_superuser',True)
+		extra_fields.setdefault('is_active', True)
+
+		if extra_fields.get('is_staff') is not True:
+			raise ValueError('Superuser must have is_staff=True.')
+		if extra_fields.get('is_superuser')is not True:
+			raise ValueError('Superuser must have superuser=True.')
+		return self.create_user(email, password, **extra_fields)
