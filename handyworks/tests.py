3b992e0f043769a08947c0b24c1b12cd86dd0fad
denn-is-njeruh <gitonjeruh@gmail.com>
2021-10-01 07:44:35 +0300

    add testcase for the creating user method on the UserManager class

--- handyworks/tests.py
+++ handyworks/tests.py
@@ -1,3 +1,23 @@
+from django.contrib.auth import get_user_model
+from django.db.models.expressions import Value
 from django.test import TestCase
 
 # Create your tests here.
+class UserManagerTest(TestCase):
+	def test_create_user(self):
+		User = get_user_model()
+		user = User.objects.create_user(email='carpenter@user.com', password='foo')
+		self.assertEqual(user.email, 'carpenter@user.com')
+		self.assertTrue(user.is_active)
+		self.assertFalse(user.is_staff)
+		self.assertFalse(user.is_superuser)
+		try:
+			self.assertIsNone()(user.username)
+		except AttributeError:
+			pass
+		with self.assertRaises(TypeError):
+			User.objects.create_user()
+		with self.assertRaises(TypeError):
+			User.objects.create_user(email='')
+		with self.assertRaises(ValueError):
+			User.objects.create_user(email='', password="foo")
